9ed7e123ad9c8d0fee08e7d4cfaa5c40fe2f9049
rahem sorour <sorourrahem17@gmail.com>
2023-07-24 04:03:23 +0100

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution in this file!
+const employee={
+  nam:'sorour',
+  streetAddress:'CITY 1200 BATNA',
+}
+
+// returns an employee with the original key value pairs and the new key value pair
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+    const updatedEmployee = { ...employee };
+
+    updatedEmployee[key] = value;
+
+    return updatedEmployee;
+  }
+  
+// test update
+  const updatedEmployee1 = updateEmployeeWithKeyAndValue(employee, 'nam', 'Sam');
+
+  const updatedEmployee2 = updateEmployeeWithKeyAndValue(updatedEmployee1, 'streetAddress', '11 Broadway');
+
+  console.log(updatedEmployee2);
+
+  console.log(employee)
+
+// updates `employee` with the given `key` and `value` and returns the entire updated employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[key] = value;
+
+    return employee;
+  }
+
+// 'deletes `key` from a clone of employee and returns the new employee without modify the original employee
+function deleteFromEmployeeByKey(employee, key) {
+    
+    const updatedEmployee = { ...employee };
+
+    delete updatedEmployee[key];
+  
+    return updatedEmployee;
+  }
+
+// returns employee without the deleted key/value pair and modifies the original employee
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    delete employee[key];
+  
+    return employee;
+  }
+
